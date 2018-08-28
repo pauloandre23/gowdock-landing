fd05cafbc29c01bf04d2507a9c9597268a7857d4
Rodrigo Lopes de Carvalho <rodrigo@Rodrigos-MacBook-Pro.local>
2018-08-28 13:20:54 -0300

    adicionados arquivos de configuracao do kubernetes

--- gowdock_landing/settings.py
+++ gowdock_landing/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'gowdock_landing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gowdock_landing',
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
-
+print (os.getenv('POSTGRES_USER'))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
