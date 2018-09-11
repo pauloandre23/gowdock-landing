cc4f610a665f414d27428e9b40e4107ab50fada6
Rodrigo Lopes <rodrigolpscarvalho@gmail.com>
2018-09-11 20:46:52 -0300

    adicionadas mensagens de erro para o model do cadastro

--- landing_page/models.py
+++ landing_page/models.py
@@ -3,8 +3,21 @@ from django.db import models
 # Create your models here.
 
 class CadastroLanding(models.Model):
-    nome = models.CharField(max_length=120)
-    email = models.EmailField(unique=True)
+    nome = models.CharField(
+        max_length=120,
+        error_messages={
+            'blank': "Nome vazio"
+        }
+    )
+    email = models.EmailField(
+        unique=True,
+        error_messages={
+            'null': "Email nulo",
+            'blank': "Email vazio",
+            'invalid': "Email inválido",
+            'unique': "Email já cadastrado"
+        }
+    )
 
     def __str__(self):
         return "%s - %s" % (self.nome,self.email)
--- landing_page/views.py
+++ landing_page/views.py
@@ -6,14 +6,16 @@ from .forms import CadastroLandingForm
 
 def index(request):
     if request.method == 'POST':
-        print("POST")
         form = CadastroLandingForm(request.POST)
         if form.is_valid():
             #se valido, salva no db e redireciona
             form.save()
             return HttpResponseRedirect('/thanks/')
+        else:
+            for field in form:
+                print (field.label,field.errors) 
+            return render(request, 'index.html', {'form_email': form,})
     else:
-        print("GET")
         form = CadastroLandingForm()
     return render(request, 'index.html', {'form_email': form})
 
