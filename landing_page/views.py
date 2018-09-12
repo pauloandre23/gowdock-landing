16c33b8a68bb1a328ddb00c6ceb47519cf510ae7
Rodrigo Lopes <rodrigolpscarvalho@gmail.com>
2018-09-12 19:03:53 -0300

    removida mensagem de debug no views.py

--- landing_page/views.py
+++ landing_page/views.py
@@ -11,10 +11,6 @@ def index(request):
             #se valido, salva no db e redireciona
             form.save()
             return HttpResponseRedirect('/thanks/')
-        else:
-            for field in form:
-                print (field.label,field.errors) 
-            return render(request, 'index.html', {'form_email': form,})
     else:
         form = CadastroLandingForm()
     return render(request, 'index.html', {'form_email': form})
